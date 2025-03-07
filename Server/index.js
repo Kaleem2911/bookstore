7ada58bac1d8e1c28344febd9f19996068a59daf
kaleem2911 <abdulkalem2911@gmail.com>
2025-03-07 12:25:44 +0530

    Changes

--- Server/index.js
+++ Server/index.js
@@ -11,11 +11,13 @@ const cart = require('./Routes/cart')
 const order = require('./Routes/order')
 
 
+const cors = require("cors");
+
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: ["http://localhost:3000", "https://your-vercel-app.vercel.app"], 
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization", "id","bookid"],
+  allowedHeaders: ["Content-Type", "Authorization", "id", "bookid"],
 }));
 
 
