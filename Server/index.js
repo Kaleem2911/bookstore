8514122259ec9f415707cf1b289bee4f636603d1
kaleem2911 <abdulkalem2911@gmail.com>
2025-03-11 16:17:39 +0530

    changes

--- Server/index.js
+++ Server/index.js
@@ -12,7 +12,7 @@ const order = require('./Routes/order')
 
 app.use(
   cors({
-    origin: "", 
+     origin: "*", credentials: true, 
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true 
